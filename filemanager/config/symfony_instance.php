fcf07f424b2b0beb3decbf3f1216f65e10985a29
Gianni Prosperi <gianni.prosperi@differentweb.it>
2017-12-13 16:06:17 +0100

    Removed logs

--- filemanager/config/symfony_instance.php
+++ filemanager/config/symfony_instance.php
@@ -7,14 +7,12 @@ $environment = "prod";
 $symfony_loaded = false;
 
 if (file_exists($base_load_file) && @include_once($base_load_file)) {
-    error_log("sono qui");
     if (!sfContext::hasInstance()) {
         $configuration = ProjectConfiguration::getApplicationConfiguration($application, $environment, false);
         sfContext::createInstance($configuration);
     }
     $symfony_loaded = true;
 } else {
-    error_log("sono quo");
     if (session_id() == '') {
         session_start();
     }
